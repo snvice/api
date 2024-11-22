47e072ebc6e5989ab97b5169a860c099b96500f5
snvice <131165228+snvice@users.noreply.github.com>
2024-11-22 03:22:47 +0300

    Add files via upload

--- database.py
+++ database.py
@@ -1,7 +1,12 @@
 from sqlmodel import create_engine
-
-DATABASE_URL = f"postgresql+psycopg2://vicee:vicee@heroes_db:5432/heroes_db"
+import os 
+# DATABASE_URL = f"postgresql+psycopg2://vicee:vicee@heroes_db:5432/heroes_db"
+# ... existing code ...
+DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
+# ... existing code ...
 
 
